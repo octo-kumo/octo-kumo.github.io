0cdb8979a51e6b4f22ac483829490bbb2496afc7
octo-kumo <octo-kumo@users.noreply.github.com>
2023-06-27 02:50:23 +0000

    deploy: 6b2f5a4904992355f4af8177f37fd59e05b7df56

--- assets/JsonSchemaConvert-587531bf.js
+++
@@ -1,9 +0,0 @@
-import{d as _,r as d,o as u,c as m,w as v,v as $,a as o,t as f}from"./index-2536c366.js";const S={class:"flex flex-wrap h-screen"},N=["textContent"],C=_({__name:"JsonSchemaConvert",setup(w){const n=e=>e.toLowerCase().replace(/([-_][a-z])/g,s=>s.toUpperCase().replace("-","").replace("_",""));function c(e){const s=new RegExp("(?<=\\n|^)\\s*([\\w_]+): (true|false)(,|$)","g"),t=new RegExp("(?<=\\n|^)\\s*([\\w_]+): \\d+(,|$)","g"),i=new RegExp(`(?<=\\n|^)\\s*([\\w_]+): ('.*'|".*")(,|$)`,"g"),p=new RegExp("(?<=\\n|^)\\s*([\\w_]+): (null)(,|$)","g");return e=e.replace(s,(l,a)=>`@SerializedName("${a}")
-private boolean ${n(a)};
-`),e=e.replace(t,(l,a)=>`@SerializedName("${a}")
-private int ${n(a)};
-`),e=e.replace(i,(l,a)=>`@SerializedName("${a}")
-private String ${n(a)};
-`),e=e.replace(p,(l,a)=>`@SerializedName("${a}")
-private Object ${n(a)};
-`),e}const r=d("");return(e,s)=>(u(),m("div",S,[v(o("textarea",{class:"flex-1","onUpdate:modelValue":s[0]||(s[0]=t=>r.value=t)},null,512),[[$,r.value]]),o("textarea",{class:"flex-1",textContent:f(c(r.value))},null,8,N)]))}});export{C as default};
