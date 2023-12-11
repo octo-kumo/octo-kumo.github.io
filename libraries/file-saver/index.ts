631a4e986d3fc61a6753c0d1fc714bee5aa31bd7
kumo <h1710169@nushigh.edu.sg>
2023-12-11 17:09:38 +0800

    fileSaver fix

--- libraries/file-saver/index.ts
+++ libraries/file-saver/index.ts
@@ -44,95 +44,36 @@ function bom(blob: Blob | string, opts?: FileSaverOptions | boolean) {
 // We just need to check for 'Safari' because all other browsers (besides Firefox) include that too
 // https://www.whatismybrowser.com/guides/the-latest-user-agent/macos
 const isMacOSWebView = _global?.navigator && /Macintosh/.test(navigator.userAgent) && /AppleWebKit/.test(navigator.userAgent) && !/Safari/.test(navigator.userAgent);
-export const saveAs = 'download' in HTMLAnchorElement.prototype && !isMacOSWebView ?
-    function saveAs(blob: Blob | string, name: string, opts?: FileSaverOptions) {
-        // Namespace is used to prevent conflict w/ Chrome Poper Blocker extension (Issue #561)
-        const a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a') as HTMLAnchorElement;
-        name = name || (typeof blob !== "string" && blob.name) || 'download'
 
-        a.download = name
-        a.rel = 'noopener' // tabnabbing
+export function saveAs(blob: Blob | string, name: string, opts?: FileSaverOptions) {
+    // Namespace is used to prevent conflict w/ Chrome Poper Blocker extension (Issue #561)
+    const a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a') as HTMLAnchorElement;
+    name = name || (typeof blob !== "string" && blob.name) || 'download'
 
-        if (typeof blob === 'string') {
-            // Support regular links
-            a.href = blob
-            if (a.origin !== location.origin) {
-                corsEnabled(a.href)
-                    ? download(blob, name, opts)
-                    : click(a, a.target = '_blank')
-            } else {
-                click(a)
-            }
+    a.download = name
+    a.rel = 'noopener' // tabnabbing
+
+    if (typeof blob === 'string') {
+        // Support regular links
+        a.href = blob
+        if (a.origin !== location.origin) {
+            corsEnabled(a.href)
+                ? download(blob, name, opts)
+                : click(a, a.target = '_blank')
         } else {
-            // Support blobs
-            a.href = URL.createObjectURL(blob)
-            setTimeout(function () {
-                URL.revokeObjectURL(a.href)
-            }, 4E4) // 40s
-            setTimeout(function () {
-                click(a)
-            }, 0)
+            click(a)
         }
-    } : 'msSaveOrOpenBlob' in navigator
-        ? function saveAs(blob: Blob | string, name: string, opts?: FileSaverOptions) {
-            name = name || (typeof blob !== "string" && blob.name) || 'download'
-
-            if (typeof blob === 'string') {
-                if (corsEnabled(blob)) {
-                    download(blob, name, opts)
-                } else {
-                    var a = document.createElement('a')
-                    a.href = blob
-                    a.target = '_blank'
-                    setTimeout(function () {
-                        click(a)
-                    })
-                }
-            } else {
-                // @ts-ignore
-                navigator.msSaveOrOpenBlob(bom(blob, opts), name)
-            }
-        }
-
-        // Fallback to using FileReader and a popup
-        : function saveAs(blob: Blob | string, name: string, opts?: FileSaverOptions, popup?: Window) {
-            // Open a popup immediately do go around popup blocker
-            // Mostly only available on user interaction and the fileReader is async so...
-            popup = popup || (open('', '_blank') ?? undefined)
-            if (popup) {
-                popup.document.title =
-                    popup.document.body.innerText = 'downloading...'
-            }
-
-            if (typeof blob === 'string') return download(blob, name, opts)
-
-            const force = blob.type === 'application/octet-stream';
-            const isSafari = /constructor/i.test(String(_global?.HTMLElement)) || _global?.safari;
-            const isChromeIOS = /CriOS\/\d+/.test(navigator.userAgent);
-
-            if ((isChromeIOS || force && isSafari || isMacOSWebView) && typeof FileReader !== 'undefined') {
-                // Safari doesn't allow downloading of blob URLs
-                const reader = new FileReader();
-                reader.onloadend = function () {
-                    let url = reader.result;
-                    if (!url) return
-                    if (typeof url !== "string") url = new TextDecoder().decode(url);
-                    url = isChromeIOS ? url : url.replace(/^data:[^;]*;/, 'data:attachment/file;')
-                    if (popup) popup.location.href = url
-                    else location.href = url
-                    popup = undefined // reverse-tabnabbing #460
-                }
-                reader.readAsDataURL(blob)
-            } else {
-                const url = URL.createObjectURL(blob);
-                if (popup) popup.location = url
-                else location.href = url
-                popup = undefined // reverse-tabnabbing #460
-                setTimeout(function () {
-                    URL.revokeObjectURL(url)
-                }, 4E4) // 40s
-            }
-        };
+    } else {
+        // Support blobs
+        a.href = URL.createObjectURL(blob)
+        setTimeout(function () {
+            URL.revokeObjectURL(a.href)
+        }, 4E4) // 40s
+        setTimeout(function () {
+            click(a)
+        }, 0)
+    }
+}
 
 function download(url: string, name: string, opts?: FileSaverOptions) {
     const xhr = new XMLHttpRequest();
