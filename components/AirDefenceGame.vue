4f7082a323e8aab4eec3e8c7989558911ca66348
kumo <h1710169@nushigh.edu.sg>
2023-12-13 18:24:17 +0800

    three js + terrain + ocean

---
+++ components/AirDefenceGame.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import {AirDefence} from "@/games/air-defence";
+import type {Ref} from "@vue/reactivity";
+
+const game_container: Ref<HTMLElement | undefined> = ref(undefined);
+watch(game_container, value => {
+  console.log("watch", value)
+  const game = new AirDefence();
+  game.attach(value);
+});
+onMounted(() => {
+});
+</script>
+
+<template>
+  <div id="air-defence-game-parent" ref="game_container"></div>
+</template>
+
+<style scoped lang="css">
+
+</style>
---
+++ pages/projects/air-defence.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AirDefenceGame from "@/components/AirDefenceGame.vue";
+import {WebGL} from "three/examples/jsm/Addons";
+</script>
+
+<template>
+  <div style="height: calc(100vh - 64px);">
+    <client-only>
+      <AirDefenceGame v-if="WebGL.isWebGLAvailable()" style="height: 100%">
+      </AirDefenceGame>
+      <v-card v-else>
+        <v-card-title>WebGL not supported!</v-card-title>
+        <v-card-text>Please use some other web browser that supports webgl</v-card-text>
+      </v-card>
+    </client-only>
+  </div>
+</template>
+
+<style scoped lang="css">
+
+</style>
