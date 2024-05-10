414caccfec9ee345918c74421d2d1d674e11c29b
kumo <h1710169@nushigh.edu.sg>
2024-05-09 23:38:56 -0400

    test new action

---
+++ mixins/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBKfHLvHOxpQwsa-zDSDiOn-PFdRo7mTBs",
+    authDomain: "octo-kumo.firebaseapp.com",
+    projectId: "octo-kumo",
+    storageBucket: "octo-kumo.appspot.com",
+    messagingSenderId: "785959296166",
+    appId: "1:785959296166:web:06d636005035b36ebfa867"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
