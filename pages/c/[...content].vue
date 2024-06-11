17e1dbc4e74aa8e9fd90a60b5069fc2b74d436c2
kumo <h1710169@nushigh.edu.sg>
2024-06-11 00:15:54 -0400

    robots + not found

--- pages/c/[...content].vue
+++ pages/c/[...content].vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import TableOfContents from "~/components/TableOfContents.vue";
 import type Node from "element-plus/es/components/tree/src/model/node";
-import type {TreeNodeData} from "element-plus/es/components/tree/src/tree.type";
-import type {NavItem} from "@nuxt/content/types";
+import type { TreeNodeData } from "element-plus/es/components/tree/src/tree.type";
+import type { NavItem, ParsedContent } from "@nuxt/content/types";
 
 const color = useColorMode();
 const path = useRoute().path.substring(2) || "/";
@@ -30,19 +30,19 @@ onActivated(() => {
 //   affixRef.value?.update();
 //   affixRef.value?.updateRoot();
 // });
-const {data: doc} = await useAsyncData(`c/doc_${path}`, () => queryContent(path).findOne());
-const {data: docs} = await useAsyncData(`c/docs_${path}`, () => queryContent("/")
-    .where({_path: {$ne: path}})
-    .only(['_path', 'title', 'description'])
-    .find());
-const {data: navigation} = await useAsyncData(`c/nav_${path}`, () => fetchContentNavigation(queryContent("/")).then(r => r.map(removeSame)));
+const { data: doc } = await useAsyncData(`c/doc_${path}`, () => queryContent(path).findOne());
+const { data: docs } = await useAsyncData(`c/docs_${path}`, () => queryContent("/")
+  .where({ _path: { $ne: path } })
+  .only(['_path', 'title', 'description'])
+  .find());
+const { data: navigation } = await useAsyncData(`c/nav_${path}`, () => fetchContentNavigation(queryContent("/")).then(r => r.map(removeSame)));
 
 function removeSame(parent: NavItem) {
   if (parent.children) (parent.children = parent.children.filter(c => c._path !== parent._path)).forEach(c => removeSame(c));
   return parent;
 }
 
-useContentHead(doc!);
+useContentHead(doc as Ref<ParsedContent>);
 definePageMeta({
   title: 'Content',
   disableSEO: true
@@ -50,59 +50,48 @@ definePageMeta({
 
 const defaultProps = {
   children: 'children',
-  label: (data: TreeNodeData, node: Node) => data.title || data._path,
+  label: (data: TreeNodeData, node: Node) => data.title || data._path.split("/").findLast(Boolean),
 };
 </script>
 <template>
-  <el-breadcrumb separator="/" class="m-2 text-center" v-if="path&&path!=='/'">
-    <el-breadcrumb-item
-        v-for="b in breadcrumbs($route.path)"
-        :to="{ path:b.path }">{{ b.name }}
+  <el-breadcrumb separator="/" class="m-2 text-center" v-if="path && path !== '/'">
+    <el-breadcrumb-item v-for="b in breadcrumbs($route.path)" :to="{ path: b.path }">{{ b.name }}
     </el-breadcrumb-item>
   </el-breadcrumb>
   <template v-if="doc">
     <article class="flex flex-row">
-      <ContentRenderer :value="doc" class="content max-w-full lg:max-w-prose lg:mx-a lg:min-w-prose"/>
+      <ContentRenderer :value="doc" class="content max-w-full lg:max-w-prose lg:mx-a lg:min-w-prose">
+        <template #empty>
+          <el-empty description="No folder note" class="h-32 flex-auto" :image-size="80" />
+        </template>
+      </ContentRenderer>
       <div class="pt-20">
-        <el-affix ref="affixRef" :offset="60" v-if="(doc?.body?.toc?.links?.length??0)>0" class="<xl:hidden!">
+        <el-affix ref="affixRef" :offset="60" v-if="(doc?.body?.toc?.links?.length ?? 0) > 0" class="<xl:hidden!">
           <el-anchor :offset="60" style="width: 250px">
-            <table-of-contents
-                v-for="child in (doc?.body?.toc?.links??[])"
-                :link="child">
+            <table-of-contents v-for="child in (doc?.body?.toc?.links ?? [])" :link="child">
             </table-of-contents>
           </el-anchor>
         </el-affix>
       </div>
     </article>
   </template>
-  <template v-if="docs&&docs.length>0">
-    <el-tree
-        class="max-w-full lg:max-w-prose lg:mx-a lg:w-prose text-base!"
-        :current-node-key="path"
-        node-key="_path"
-        highlight-current
-        auto-expand-parent
-        :default-expanded-keys="[path]"
-        :data="navigation!"
-        :props="defaultProps"
-    >
+  <template v-if="docs && docs.length > 0">
+    <el-tree class="max-w-full lg:max-w-prose lg:mx-a lg:w-prose text-base!" :current-node-key="path" node-key="_path"
+      highlight-current auto-expand-parent :default-expanded-keys="[path]" :data="navigation!" :props="defaultProps">
       <template #default="{ node, data }">
         <span class="flex justify-between flex-1">
-          <nuxt-link :to="'/c'+data._path">{{ node.label || data._path }}</nuxt-link>
+          <nuxt-link :to="'/c' + data._path">{{ node.label }}</nuxt-link>
           <span>{{ docs.find(d => d._path === data._path)?.description }}</span>
         </span>
       </template>
     </el-tree>
   </template>
-  <utterances v-if="doc||(docs&&docs.length>0)"
-              repo="octo-kumo/octo-kumo.github.io"
-              issue-term="pathname"
-              label="utteranc"
-              :theme="color.value === 'dark'?'github-dark':'github-light'"/>
+  <utterances v-if="doc || (docs && docs.length > 0)" repo="octo-kumo/octo-kumo.github.io" issue-term="pathname"
+    label="utteranc" :theme="color.value === 'dark' ? 'github-dark' : 'github-light'" />
   <template v-else>
-    <el-empty description="Not found"/>
+    <el-empty description="Not found" />
   </template>
-  <el-backtop :right="50" :bottom="50"/>
+  <el-backtop :right="50" :bottom="50" />
 </template>
 <style lang="scss">
 .content {
@@ -117,7 +106,12 @@ const defaultProps = {
     border-radius: var(--el-border-radius-base);
   }
 
-  h1, h2, h3, h4, h5, h6 {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
     scroll-margin-top: 60px;
   }
 
