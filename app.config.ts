8ae867d5031b03d71e9ac33d1c73879badb70d6c
kumo <zy.kumo@gmail.com>
2024-06-25 22:40:54 -0400

    i give up

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,7 @@ export default defineAppConfig({
     umami: {
         host: "https://analytics.octo-kumo.me",
         id: 'fd089b30-9952-41b0-a817-fce38fefae17',
-        version: 2
+        version: 2,
+        debug: true
     },
 });
