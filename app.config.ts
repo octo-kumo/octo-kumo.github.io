1e16fe721423cb01fc9c35d8e8d1f43f99f35242
kumo <zy.kumo@gmail.com>
2024-06-25 22:43:21 -0400

    i give up

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ export default defineAppConfig({
         host: "https://analytics.octo-kumo.me",
         id: 'fd089b30-9952-41b0-a817-fce38fefae17',
         version: 2,
-        debug: true
+        debug: true,
+        customEndpoint: "/api/send"
     },
 });
