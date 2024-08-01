f3f7ea1c294cfdccdb632a732e248aba48b7584b
kumo <zy.kumo@gmail.com>
2024-08-01 15:34:46 -0400

    fix?

--- utils/query-all-docs.ts
+++ utils/query-all-docs.ts
@@ -3,26 +3,32 @@ import type { NavItem } from "@nuxt/content";
 export default async function queryAllDocs(indexPage?: boolean): Promise<{ nav: NavItem[], flat: NavItem[] }> {
     const nav = await fetchContentNavigation(queryContent("/")
         .where({ _extension: { $eq: 'md' }, ...(indexPage ? { created: { $exists: true } } : {}) }).sort({ created: -1 }));
-    return { nav: removeNavChildSelf(nav), flat: flatten(nav) };
+    return { nav: removeNavChildSelf(nav), flat: flatten(nav).map(clean) };
 }
 
 function removeNavChildSelf(items: NavItem[], _path?: string) {
     items = items.filter(c => c._path !== _path);
-    for (let item of items)
-        if (item.children) item.children = removeNavChildSelf(item.children, item._path);
+    for (let item of items) {
+        if (item.children) {
+            if (item._path === "/ctf") item.children?.sort(sorter);
+            item.children = removeNavChildSelf(item.children, item._path);
+        }
+    }
     return items;
 }
 
+function clean(item: NavItem) {
+    item = { ...item };
+    delete item.children;
+    return item;
+}
 
 function flatten(query: NavItem[]): NavItem[] {
     let all = [];
     for (let item of query) {
         item.title = guessArticleTitle(item);
-        if (item.children) {
-            if (item._path === "/ctf")
-                item.children.sort(sorter);
-            all.push(...flatten(item.children));
-        } else all.push(item)
+        if (item.children) all.push(...flatten(item.children));
+        all.push(item);
     }
     return all;
 }
