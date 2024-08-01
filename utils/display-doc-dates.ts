78da6cbf2c4b359ac2ef82524dcd2060b0d93694
kumo <zy.kumo@gmail.com>
2024-08-01 19:44:04 -0400

    updated git

--- utils/display-doc-dates.ts
+++ utils/display-doc-dates.ts
@@ -1,6 +1,6 @@
 import type { NavItem, ParsedContent } from "@nuxt/content";
 
-export default function displayDocDates(doc?: ParsedContent | NavItem | { created: string, updated: string }) {
+export default function displayDocDates(doc?: ParsedContent | NavItem | { created?: string, updated?: string }) {
     if (!doc) return '';
     const a = displayDatetime(doc.created);
     const b = displayDatetime(doc.updated);
