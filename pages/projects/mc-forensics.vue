29f0faae35a7d6e60ca5b0b1cfb445465f09eb48
kumo <zy.kumo@gmail.com>
2024-08-18 23:53:19 -0400

    E

---
+++ pages/projects/mc-forensics.vue
@@ -0,0 +1,10 @@
+<template>
+  <github-repo repo="octo-kumo/mc-forensics" />
+</template>
+<script setup lang="ts">
+definePageMeta({
+  title: "Minecraft Forensics Tool",
+  description: "Tool to help with analyzing minecraft save files",
+  // image: "https://raw.githubusercontent.com/octo-kumo/evil-er/master/res/images/icons/icon.svg"
+});
+</script>
