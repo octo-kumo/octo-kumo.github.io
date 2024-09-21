df7779f95713adfdf3ed00981c8093d6e94bc050
kumo <zy.kumo@gmail.com>
2024-09-21 00:11:38 -0400

    hooray

---
+++ pages/projects/tank.vue
@@ -0,0 +1,10 @@
+<template>
+  <github-repo repo="octo-kumo/Tank" />
+</template>
+<script setup lang="ts">
+definePageMeta({
+  title: "Tank Field",
+  description: "Clone of Tank Stars! Supports PVP and terrain destruction!",
+  image: "https://raw.githubusercontent.com/octo-kumo/Tank/refs/heads/master/images/img.webp"
+});
+</script>
