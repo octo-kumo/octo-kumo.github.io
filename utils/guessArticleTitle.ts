8262ef360b3029ab644d2ec362108bc61e7b20dc
kumo <zy.kumo@gmail.com>
2024-11-13 15:38:33 -0500

    fancier date format

--- utils/guessArticleTitle.ts
+++ utils/guessArticleTitle.ts
@@ -1,5 +1,13 @@
-import type {NavItem, ParsedContent} from "@nuxt/content";
+import type { NavItem, ParsedContent } from "@nuxt/content";
 
 export default function (item?: ParsedContent | NavItem | Partial<ParsedContent> | null): string {
+    if (item?.title && item?.tags?.includes('blog') && !isNaN(new Date(item?.title).getTime())) {
+        return new Date(item?.title).toLocaleDateString(undefined, {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            weekday: 'short',
+        });
+    }
     return item?.title || item?._path?.split('/')?.pop() || 'Unknown';
 }
