e0ce5cc0bdb862dd2f669b2d665062756a794eee
kumo <zy.kumo@gmail.com>
2024-12-09 00:43:42 -0500

    updated title

--- content/index.md
+++ content/index.md
@@ -1,6 +1,6 @@
 ---
 created: 2024-06-08T18:38
-updated: 2024-08-16T16:54
+updated: 2024-12-09T00:34
 title: Content Page
 ---
 
@@ -12,3 +12,9 @@ Hello!
 ```js [src/index.js]
 console.log("Hello world!")
 ```
+
+## [Blog](/c/blog)
+A blog, nothing much currently. Might be useful in the future.
+
+## [CTF Writeups](/c/ctf)
+All of my CTF writeups.
--- utils/guessArticleTitle.ts
+++ utils/guessArticleTitle.ts
@@ -1,8 +1,7 @@
 import type { NavItem, ParsedContent } from "@nuxt/content";
 
 export default function (item?: ParsedContent | NavItem | Partial<ParsedContent> | null): string {
-    if (item?.title && item?.tags?.includes('blog') && !isNaN(new Date(item?.title).getTime())) {
-        // return item?.title ?? '';
+    if (item?.title && item?.tags?.includes('blog') && /^\d{4}-\d{2}-\d{2}$/.test(item?.title ?? "")) {
         return new Date(item?.title).toLocaleDateString(undefined, {
             timeZone: 'UTC',
             year: 'numeric',
