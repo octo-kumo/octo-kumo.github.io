f3f03307af6825f8b29da2ac76e1783bbe3278f8
octo-kumo <yun@yun.ng>
2026-03-28 18:14:58 -0400

    fix links prefetch

--- app/components/KumoLink.vue
+++ app/components/KumoLink.vue
@@ -1,8 +1,9 @@
 <template>
-  <nuxt-link custom v-slot="routerProps">
+  <nuxt-link custom v-slot="routerProps" :no-prefetch="Boolean($attrs.noPrefetch)">
     <el-link
         :type="routerProps.isActive?'primary':'default'"
         :href="routerProps.href"
+        :target="Boolean($attrs.noPrefetch)?'_blank':'_self'"
         v-bind="$attrs"
         @click="routerProps.navigate">
       <slot/>
